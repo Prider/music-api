54c2c0bfd1bb53b223e452c11babc7a6c06b6b5b
pawat <pchomphoosang@gmail.com>
2020-12-01 13:26:46 +0700

    test

--- musicApi.py
+++ musicApi.py
@@ -1,20 +1,23 @@
 from flask import Flask, request
-from flask_json import json_response
+from datetime import datetime
+from flask_json import FlaskJSON, JsonError, json_response, as_json
 
 app = Flask(__name__)
+FlaskJSON(app)
 
 @app.route('/')
 def index():
-    return 'music api is working'
+    now = datetime.utcnow()
+    return json_response(time=now)
 
 @app.route('/music', methods=['POST','GET'])
 def music():
     file = request.args.get('file')
     try:
         newFile = int(file)
-        return json_response(success=True, file=newFile)
-    except:
-        return json_response(error=True, messsage='invalid file')
+        return json_response(value=newFile)
+    except (KeyError, TypeError, ValueError):
+        raise JsonError(description='Invalid value.')
 
  
 if __name__ == '__main__':
